14cff6c9f4da6696ff923f6eb8064972888ace66
FzHars <aljawif1@gmail.com>
2024-01-16 11:55:33 +0700

    Tambah list pada file php

--- index.php
+++ index.php
@@ -8,5 +8,17 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis exercitationem dolore! Perspiciatis fugiat velit tempora consectetur autem nisi accusamus consequuntur. Repellat pariatur nobis corrupti facilis doloremque voluptatem illo quaerat.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+        <li>10</li>
+    </ul>
 </body>
 </html>
