65942d3c7a6263bff4477bd2381f98f811d9fd6e
FzHars <aljawif1@gmail.com>
2024-01-16 11:43:18 +0700

    Menambahkan paragraf pada filr php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem facilis exercitationem dolore! Perspiciatis fugiat velit tempora consectetur autem nisi accusamus consequuntur. Repellat pariatur nobis corrupti facilis doloremque voluptatem illo quaerat.</p>
 </body>
 </html>
